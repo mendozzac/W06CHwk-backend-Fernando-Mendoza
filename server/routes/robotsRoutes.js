ff1da2f40c705fad2913d66c533f498376302036
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-11-07 20:12:04 +0100

    Add post method

--- server/routes/robotsRoutes.js
+++ server/routes/robotsRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { getRobots, getRobotById } = require("../controller/robotsController");
+const {
+  getRobots,
+  getRobotById,
+  createRobot,
+} = require("../controller/robotsController");
 
 const router = express.Router();
 
 router.get("/", getRobots);
 
 router.get("/:idRobot", getRobotById);
+
+router.post("/create", createRobot);
 
 module.exports = router;
