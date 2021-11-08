efa170ce03bbf198f8989acfb8890c195c52e6ce
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-11-08 18:30:03 +0100

    Add userController

--- database/index.js
+++ database/index.js
@@ -4,6 +4,13 @@ const mongoose = require("mongoose");
 
 const connectDB = () =>
   new Promise((resolve, reject) => {
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret._id;
+      },
+    });
     mongoose.connect(process.env.MONGODB_STRING, (error) => {
       if (error) {
         debug(chalk.red("La base de datos, que no va."));
