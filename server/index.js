5aca5e6932f0cd9c7b308aa5eb17e336dd5443da
Fernando <fernando.isdicoder@gmail.com>
2021-12-26 00:17:00 +0000

    Change port param in db connect

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const debug = require("debug")("robots:server");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const auth = require("./middlewares/auth");
+// const auth = require("./middlewares/auth");
 const robotsRoutes = require("./routes/robotsRoutes");
 const userRoute = require("./routes/usersRoute");
 const {
@@ -32,7 +32,7 @@ const initializeServer = (port) =>
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use("/robots", auth, robotsRoutes);
+app.use("/robots", robotsRoutes);
 app.use("/users", userRoute);
 
 app.use(notFoundErrorHandler);
